be03e9d9a13b3e4b413e6395c851943055fa4fb0
Jo Oliveira <rodriguesjo8@outlook.com>
2024-05-08 20:28:27 -0300

    Projeto Criado

---
+++ src/test/java/curso/rodriguesjo8/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.rodriguesjo8.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
